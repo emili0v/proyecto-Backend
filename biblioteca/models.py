4b0bc62c538bafd22c7ebe61e3bba2386ffde5e5
emili0v <emiliov744@gmail.com>
2025-08-19 18:03:28 -0400

    Agregar modelo Biblioteca y ajustes en settings

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Biblioteca(models.Model):
+    nombre = models.CharField(max_length=100,null=False)
+    direccion = models.CharField(max_length=250,null=False)
+
+class lector(models.Model):
+    id_biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=250,null=False)
+    rut = models.IntegerField(null=False)
+    dig_verificador = models.CharField(max_length=1,null=False)
+
+class autor(models.Model):
+    nombre = models.CharField(max_length=250,null=False)
+    
