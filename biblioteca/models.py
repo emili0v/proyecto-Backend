9b49296f385ba37e5e9e1eb979d39b69cf555541
emili0v <emiliov744@gmail.com>
2025-08-26 17:13:46 -0400

    Avance en models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -11,6 +11,21 @@ class lector(models.Model):
     rut = models.IntegerField(null=False)
     dig_verificador = models.CharField(max_length=1,null=False)
 
-class autor(models.Model):
-    nombre = models.CharField(max_length=250,null=False)
+class Autor(models.Model):
+    nombre = models.CharField(max_length=250, null=False)
+    pseudonimo = models.CharField(max_length=50, null=True)
+    id_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+class Comuna(models.Model):
+    codigo = models.CharField(max_length=5, null=False)
+    comuna = models.CharField(max_length=50, null=False)
+
+
+class Direccion(models.Model):
+    id_comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    calle = models.CharField(max_length=50, null=True)
+    numero = models.CharField(max_length=10, null=True)
+    departamento = models.CharField(max_length=10, null=True)
+    detalles = models.TextField(null=True)
     
